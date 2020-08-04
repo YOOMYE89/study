9304f0a3aeefd3d88a29361d68249028a6960378
YOOMYE89 <mythdcom@naver.com>
2020-08-04 23:04:22 +0900

    src commit

---
+++ src/main/java/com/dev/sboot/devApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class devApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(devApplication.class, args);
+    }
+}
